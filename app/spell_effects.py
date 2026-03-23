54a2bd43d4f0bd88b2b4cf5774f0e273e4f01dad
Yevhen Maksymov <goldmen53@gmail.com>
2026-03-23 01:07:46 +0000

    add some spells

--- app/spell_effects.py
+++ app/spell_effects.py
@@ -3,10 +3,10 @@ from app.models import Hero,Monster
 from fastapi import FastAPI, Depends, HTTPException
 from app.database import init_db, get_session
 import random
-from app.utils import give_monster_rewards
+from app.utils import give_monster_rewards, hero_overflow_check
 
 
-def cast_fire_damage(hero ,session):
+def effect_cast_fire_ball(hero ,session):
     if not hero.active_monster_id:
         return "Здесь нет врагов, чтобы пускать огненные шары!"
     
@@ -16,14 +16,24 @@ def cast_fire_damage(hero ,session):
         hero.active_monster_id = None
         return "Монстр исчез в тени..."
 
-    damage = 10 + (hero.total_intelligence * 2) + hero.level
+    damage = 10 + (hero.total_intelligence * 3) + hero.level
     monster.current_hp -= int(damage)
-    
-    
     
     return f"Огненный шар наносит {int(damage)} урона монстру {monster.name}!"
 
-def heal_self(hero, session):
+def effect_сhain_lightning(hero ,session):
+    # Ищем монстра по ID, сохраненному у героя
+    monster = session.get(Monster, hero.active_monster_id)
+    if not monster:
+        hero.active_monster_id = None
+        return "Монстр исчез в тени..."
+
+    damage = 15 + (hero.total_intelligence * 5) + (hero.dexterity*2) + (hero.level*2)
+    monster.current_hp -= int(damage)
+    
+    return f"Громовой разряд наносит {int(damage)} урона монстру {monster.name}!"
+
+def effect_heal_self(hero, session):
     base_power = 5
     # Интеллект теперь реально влияет на силу магии!
     spell_power = base_power + (hero.total_intelligence *2) + hero.level
@@ -44,7 +54,7 @@ def heal_self(hero, session):
         f"Состояние: {hero.hp}/{hero.max_hp} HP."
     )
 
-def admin_kill(hero, session):
+def effect_admin_kill(hero, session):
     if not hero.active_monster_id:
         return "Здесь нет врагов"
     
@@ -59,12 +69,128 @@ def admin_kill(hero, session):
 
     return f"Огненный шар наносит {int(damage)} урона монстру {monster.name}!"
 
+def effect_fire_attack(hero,session):
+    monster = session.get(Monster, hero.active_monster_id)
+    # --- ХОД ГЕРОЯ ---
+    hero_damage = random.randint(10+hero.total_strength+hero.intelligence, 10+ hero.total_strength + hero.total_dexterity+hero.intelligence)
+    log = []
+
+    if random.random() <= hero.total_crit/100: 
+        damage = hero_damage * 2
+        log.append(f"Критический удар! Вы ударили огнянной атакой {monster.name} на {damage} урона.")
+    else:
+        damage = hero_damage
+        log.append(f"Вы ударили огнянной атакой {monster.name} на {damage} урона.")
+    
+    monster.current_hp -= int(damage)
+
+    return log
+
+def effect_ice_attack(hero,session):
+    monster = session.get(Monster, hero.active_monster_id)
+    # --- ХОД ГЕРОЯ ---
+    hero_damage = random.randint(10+hero.total_strength, 10+ hero.total_strength + hero.total_dexterity)
+    log = []
+
+    if random.random() <= hero.total_crit/100: 
+        damage = hero_damage * 2
+        log.append(f"Критический удар! Вы ударили ледяной атакой {monster.name} двигаеться медленее. Вы наненесли {damage}.")
+    else:
+        damage = hero_damage
+        log.append(f"Вы ударили ледяной атакой {monster.name} двигаеться медленее. Вы нанесли {damage}")
+    
+    monster.current_hp -= int(damage)
+    hero.hp += (monster.max_attack /3)
+    hero_overflow_check(hero)
+
+    return log
+
+def effect_crit_attack(hero,session):
+    monster = session.get(Monster, hero.active_monster_id)
+    # --- ХОД ГЕРОЯ ---
+    hero_damage = random.randint(10+hero.total_strength, 10+ hero.total_strength + hero.total_dexterity)
+    log = []
+    damage = hero_damage * 2
+    log.append(f"Критический удар! Вы ударили  {monster.name} на {damage} урона..")
+    
+    monster.current_hp -= int(damage)
+
+    return log
+    
+def effect_mana_surge(hero,session):
+    
+    if hero.hp <= 50:
+        return "Вы слишком слабы что б применить это заклинание"
+    else:
+
+        hero.hp -= 50
+        hero.mp += 25
+
+        hero_overflow_check(hero)
+
+        return "Вы жертвуете своим здоровьем, что б востановить часть маны"
+
+def effect_void_wrath(hero,session):
+    monster = session.get(Monster, hero.active_monster_id)
+    damage = hero.mp *10
+
+    monster.current_hp -= int(damage)
+    hero.mp = 0
+    return f"Вы выплескиваете всю свою магическую энергию. {monster.name} получает {damage} урона! Вы полонстю опустошены..."
+
+def effect_fire_spear(hero,session):
+    monster = session.get(Monster, hero.active_monster_id)
+
+    hero_damage = 20 + (hero.total_intelligence * 3) + hero.dexterity 
+    
+    log=[]
+
+    if random.random() <= hero.total_crit/100: 
+        damage = hero_damage * 2
+        log.append(f"Критический удар! Вы ударили огняным копьем {monster.name} на {damage} урона.")
+    
+    else:
+        damage = hero_damage
+        
+        log.append(f"Вы ударили огняным копьем {monster.name} на {damage} урона.")
+    
+    monster.current_hp -= int(damage)
+
+    return log
+
+def effect_magic_push(hero,session):
+    monster = session.get(Monster, hero.active_monster_id)
+
+    hero_damage = 10 + (hero.total_intelligence * 2) + hero.level*3 + hero.strength 
+    
+    log= []
+
+    if random.random() > 0.5:
+        
+        damage = hero_damage *2
+        log.append(f"Вы применили магический толчек на {monster.name} он упал и получил {damage} критического урона.")
+    else:
+        
+        damage = hero_damage
+        log.append(f"Вы применили магический толчек на {monster.name} он получил {damage} урона.")
+
+    monster.current_hp -= int(damage)
+    
+    return log
 
 
 # Реестр: связываем строку из БД с функцией
 SPELLS_EFFECTS = {
-    "deal_fire_damage": cast_fire_damage,
-    "heal_self": heal_self,
-    "deal_999999_damage": admin_kill
+    "deal_fire_damage": effect_cast_fire_ball,
+    "heal_self": effect_heal_self,
+    "deal_999999_damage": effect_admin_kill,
+    "fire_attack": effect_fire_attack,
+    "ice_attack": effect_ice_attack,
+    "crit_attack": effect_crit_attack,
+    "сhain_lightning": effect_сhain_lightning,
+    "mana_surge": effect_mana_surge,
+    "void_wrath": effect_void_wrath,
+    "fire_spear": effect_fire_spear,
+    "magic_push": effect_magic_push
     
 }
--- app/spells.py
+++ app/spells.py
@@ -24,6 +24,70 @@ PRESET_SPELLS = [
         "level": 1,
         "rarity": "admin"
     },
+    {
+        "name": "Огнянная атака",
+        "description": "Добавляет обычным атакам урон огнем ",
+        "effect_key": "fire_attack",
+        "mp_cost": 3,
+        "level": 1,
+        "rarity": "base"
+    },
+    {
+        "name": "Ледяная атака",
+        "description": "Добовляет обычным атакам замораживающий еффект враги бьют вас немного слабее",
+        "effect_key": "ice_attack",
+        "mp_cost": 3,
+        "level": 1,
+        "rarity": "base"
+    },
+    {
+        "name": "Критическая атака",
+        "description": "Гарантированая критическая атака",
+        "effect_key": "crit_attack",
+        "mp_cost": 10,
+        "level": 2,
+        "rarity": "rare"
+    },
+    {
+        "name": "Грозовой Разряд",
+        "description": "Мощный громовой разряд",
+        "effect_key": "сhain_lightning",
+        "mp_cost": 10,
+        "level": 3,
+        "rarity": "rare"
+    },
+    {
+        "name": "Магический Резонанс",
+        "description": "Вы жертвует 50 единицами здоровь , что б востановить 25 единиц маны",
+        "effect_key": "mana_surge",
+        "mp_cost": 0,
+        "level": 3,
+        "rarity": "epic"
+    },
+    {
+        "name": "Гнев Пустоты",
+        "description": "Вы полностю опустошаете свой магический резерв взамен наносите колосальный магический урон",
+        "effect_key": "void_wrath",
+        "mp_cost": 0,
+        "level": 1,
+        "rarity": "legendary"
+    },
+    {
+        "name": "Огнянное копье",
+        "description": "Выстреливает огняным копьем (может ударить критом)",
+        "effect_key": "fire_spear",
+        "mp_cost": 5,
+        "level": 2,
+        "rarity": "rare"
+    },
+    {
+        "name": "Магический толчек",
+        "description": "Толкает магческой силой. Есть шанс сбить противника с ног",
+        "effect_key": "magic_push",
+        "mp_cost": 5,
+        "level": 1,
+        "rarity": "base"
+    }
     
     
 ]
